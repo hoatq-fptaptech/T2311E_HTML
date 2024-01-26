6e94afeaedf5e17e2cb390dfe96d0abb836a244a
quanghoatrinh <quanghoa.trinh@gmail.com>
2024-01-26 19:27:30 +0700

    demo document element

---
+++ demo_js2.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Layout</title>
+    <!-- Font google -->
+    <?php include("html/css.html");?>
+</head>
+<body>
+    <main class="section main">
+        <div class="container">
+            <h1>Demo JS2</h1>
+            <h2 id="number">10</h2>
+            <h2>
+                <span id="min">10</span>
+                :
+                <span id="sec">00</span>
+            </h2>
+        </div>
+    </main>
+   <script type="text/javascript" src="js/demo2.js"></script>
+</body>
+</html>
---
+++ js/demo2.js
@@ -0,0 +1,40 @@
+// setTimeout(function(){
+//     alert("Hello everybody");
+// },2000);// delay 2000 ms
+
+var h = document.getElementById("number");
+// h.innerText = "XIN CHAO CAC BAN";
+
+var t=10;
+var it = setInterval(function(){
+    // alert("Hello T2311E");
+    // console.log(t);
+    h.innerText = t;
+    t--;
+    if(t<0){
+        clearInterval(it);
+    }
+},1000);
+
+// 10:00
+// 09:59
+// 09:58
+//....
+// 00:00
+
+var span_min = document.getElementById("min");
+var span_sec = document.getElementById("sec");
+var min = 10, sec=0;
+var timer = setInterval(function(){
+    // console.log(`${min}:${sec}`);
+    span_min.innerText = min;
+    span_sec.innerText = sec;
+    sec--;
+    if(sec<0){
+        min--;
+        sec = 59;
+    }
+    if(min<0){
+        clearInterval(timer);
+    }
+},100);
